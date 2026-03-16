dccc1c91904fd98d11bba8c794850749b0dcaa3f
Alexey Tihomirov <alexey.tihomirov7@gmail.com>
2026-03-16 09:28:54 +0200

    updated logo outline

--- src/components/benefits-section/index.tsx
+++ src/components/benefits-section/index.tsx
@@ -9,13 +9,23 @@ export function BenefitsSection(props: Partial<BenefitsSectionProps>) {
   return (
     <>
       <div className="container-padding-x mx-auto flex max-w-7xl">
-        <h1 className="heading-xl uppercase">
-          <span className="text-primary">
-            We offer advanced dry ice solutions that provide a strategic
-            advantage, tailored to the
-          </span>{" "}
-          specific needs of your industry.
-        </h1>
+        <div className="relative">
+          <img
+            src="/images/logo-outline.svg"
+            alt=""
+            width={145}
+            height={182}
+            className="pointer-events-none absolute -left-28 -top-14 z-0"
+            aria-hidden="true"
+          />
+          <h1 className="relative z-10 heading-xl uppercase">
+            <span className="text-primary">
+              We offer advanced dry ice solutions that provide a strategic
+              advantage, tailored to the
+            </span>{" "}
+            specific needs of your industry.
+          </h1>
+        </div>
       </div>
       <section className="section-padding-y bg-primary">
         <div className="container-padding-x bg-primary mx-auto flex max-w-7xl flex-col gap-10 md:gap-12">
