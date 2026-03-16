72063d1fd817627dc0494d97a102d72de6294107
Alexey Tihomirov <alexey.tihomirov7@gmail.com>
2026-03-16 15:24:41 +0200

    updated - config

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,14 +3,13 @@
 import tailwindcss from "@tailwindcss/vite";
 import { defineConfig } from "astro/config";
 import react from "@astrojs/react";
-import svgr from "vite-plugin-svgr";
 
 // https://astro.build/config
 export default defineConfig({
   output: "static",
 
   vite: {
-    plugins: [tailwindcss(), svgr()],
+    plugins: [tailwindcss()],
     optimizeDeps: {
       include: ["react-dom/client"],
     },
